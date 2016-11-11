a8bdbb6a1b03c04b91fb7cc8ebfc1989de0d38d3
hajing <abigail67@sina.com>
2016-11-12 01:58:46 +0800

    按STAR法则修改简历

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,19 @@ $(function () {
     $(".experience-four").mouseover(function () {$("#col_3").css("fontWeight","bold");}).mouseout(function () {$("#col_3").css("fontWeight","normal");});
     $(".experience-five").mouseover(function () {$("#col_4").css("fontWeight","bold");}).mouseout(function () {$("#col_4").css("fontWeight","normal");});
     $(".experience-six").mouseover(function () {$("#col_5").css("fontWeight","bold");}).mouseout(function () {$("#col_5").css("fontWeight","normal");});
-
+    // 项目节点提示语淡入淡出
     $("#tips").fadeIn(1000).fadeOut(1000).fadeIn(2000).fadeOut(1000);
+
+    //  个人经历处各个项目轮播
+    $('.test-class').slick({
+        autoplay: true,
+        dots: true,
+        // dotsClass: 'slick_dots',
+        // appendDots: $('.slick_dots'),
+        pauseOnDotsHover: true,
+        draggable: false,
+        autoplaySpeed: 4000,
+    });
+
 
 });
